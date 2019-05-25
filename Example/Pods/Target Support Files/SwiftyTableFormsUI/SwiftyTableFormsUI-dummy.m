9ac358330ea2ad97969a0d00d1f42542ac1b4570
NoodleOfDeath <git@noodleofdeath.com>
2019-05-25 13:22:34 -0400

    Cleared git cache

--- Example/Pods/Target Support Files/SwiftyTableFormsUI/SwiftyTableFormsUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftyTableFormsUI : NSObject
-@end
-@implementation PodsDummy_SwiftyTableFormsUI
-@end
--- Example/Pods/Target Support Files/SwiftyTextStyles/SwiftyTextStyles-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftyTextStyles : NSObject
-@end
-@implementation PodsDummy_SwiftyTextStyles
-@end
